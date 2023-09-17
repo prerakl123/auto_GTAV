0c2fb2fbf66ee7050bb855c753ddbacd7c104a87
Prerak <prerakl123@gmail.com>
2023-09-17 19:23:24 +0530

    Add initial files. Add streaming the screen to cv2. Add comments.
    Signed-off-by: Prerak <prerakl123@gmail.com>

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+# Import Modules
+import time
+
+import cv2
 import numpy as np
 from PIL import ImageGrab
-import cv2
-import time
 
 last_time = time.time()
 while True:
+    # Capture the screen (800x640)
     screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
-    # printscreen_numpy = np.array(screen.getdata(), dtype='uint8')
-    #     .reshape((screen.size[1], screen.size[0], 3))
 
+    # Debugging line for execution time
     print('{} seconds'.format(time.time() - last_time))
     last_time = time.time()
+
+    # Show the captured screen in the OpenCV window
     cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+
+    # Exit config
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break

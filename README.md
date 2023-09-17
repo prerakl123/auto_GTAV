1b3240dcbec19e559fd9199c6ee410b6116f3b46
Prerak <prerakl123@gmail.com>
2023-09-17 20:02:26 +0530

    Add README.md Add Edge Detection
    Signed-off-by: Prerak <prerakl123@gmail.com>

---
+++ README.md
@@ -0,0 +1,2 @@
+# auto_GTAV
+Repo for self driving cars in GTA-V using Neural Networks and Deep Learning.
--- main.py
+++ main.py
@@ -5,17 +5,26 @@ import cv2
 import numpy as np
 from PIL import ImageGrab
 
+
+def process_img(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    return processed_img
+
+
 last_time = time.time()
 while True:
     # Capture the screen (800x640)
-    screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
+    screen = np.array(ImageGrab.grab(bbox=(0, 177, 941, 708)))
+    new_screen = process_img(original_image=screen)
 
     # Debugging line for execution time
     print('{} seconds'.format(time.time() - last_time))
     last_time = time.time()
 
     # Show the captured screen in the OpenCV window
-    cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    cv2.imshow('window', new_screen)
+    # cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
 
     # Exit config
     if cv2.waitKey(25) & 0xFF == ord('q'):

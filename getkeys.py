84aeae53ed1e0949e0b4f960c0ef89d6fc9cbad5
Prerak <prerakl123@gmail.com>
2023-09-18 00:24:41 +0530

    Add getkeys.py for keypress detection.
    Signed-off-by: Prerak <prerakl123@gmail.com>

---
+++ getkeys.py
@@ -0,0 +1,13 @@
+import  win32api as wapi
+
+key_list = ["\b"]
+for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890,.'$/\\":
+    key_list.append(char)
+
+
+def key_check():
+    keys = []
+    for key in key_list:
+        if wapi.GetAsyncKeyState(ord(key)):
+            keys.append(key)
+    return keys
